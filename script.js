60930b2c6390e3cbb65dadbfa519aae3091286ce
shirish pandey <pandeyshirish918@gmail.com>
2025-07-13 20:01:34 +0530

    Update and rename tac.js to script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Tic Tac Toe")
 let music = new Audio("music.mp3")
-let audioTurn = new Audio("image use/ting.mp3")
+let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
